8c6852e78373d08ec5622d016d4167c4d52e099f
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-04-18 21:51:53 -0400

    El primer plugins

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,38 @@
 function MediaPlayer(config){
     this.media = config.el;
-}
+    this.plugins = config.plugins || [];
+
+    this._initPugins();
+};
+
+MediaPlayer.prototype._initPugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
 
 MediaPlayer.prototype.play = function(){
-    this.media.paused ? this.media.play() : this.media.pause();
+    this.media.play();
+};
+
+MediaPlayer.prototype.pause = function(){
+    this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function(){
+    this.media.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
 }
+
+MediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+}
+
+MediaPlayer.prototype.toggleMute = function(){
+    this.media.muted ? this.unmute() : this.mute();
+};
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,12 @@
-import MediaPlayer from './MediaPlayer.js'
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({el: video});
-const button = document.querySelector("button");
+const player = new MediaPlayer({el: video, plugins: [
+    new AutoPlay()
+]});
+const btnPlayPause = document.querySelector("#btnPlayPause");
+const btnMuteUnmute = document.querySelector("#btnMuteUnmute");
 
-button.onclick = () => player.play();
+btnPlayPause.onclick = () => player.togglePlay();
+btnMuteUnmute.onclick = () => player.toggleMute();
