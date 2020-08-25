b1e1d8212638712b8d5085acfb16591c9b79772d
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-08-24 21:00:33 -0400

    added this.html

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,10 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({el: video, plugins: [
-    new AutoPlay()
-]});
+const player = new MediaPlayer({el: video, 
+    plugins: [
+    // new AutoPlay()
+    ]});
 const btnPlayPause = document.querySelector("#btnPlayPause");
 const btnMuteUnmute = document.querySelector("#btnMuteUnmute");
 
