4de3da4ce6df2ccb22b3a3a7736f0705ccedd3b9
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-08-24 21:01:35 -0400

    did format to index.js

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,12 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({el: video, 
+const player = new MediaPlayer({
+    el: video,
     plugins: [
-    // new AutoPlay()
-    ]});
+        //  new AutoPlay()
+    ]
+});
 const btnPlayPause = document.querySelector("#btnPlayPause");
 const btnMuteUnmute = document.querySelector("#btnMuteUnmute");
 
