3172e4e836132becc27b5d3e9f9b2799bb40072c
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-09-11 22:37:58 -0400

    Added getter and setter at MediaPlayer.js

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,38 +1,51 @@
-function MediaPlayer(config){
-    this.media = config.el;
-    this.plugins = config.plugins || [];
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
 
-    this._initPugins();
-};
-
-MediaPlayer.prototype._initPugins = function() {
-    this.plugins.forEach(plugin => {
-        plugin.run(this);
-    });
-};
-
-MediaPlayer.prototype.play = function(){
-    this.media.play();
-};
-
-MediaPlayer.prototype.pause = function(){
-    this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function(){
-    this.media.paused ? this.play() : this.pause();
-};
-
-MediaPlayer.prototype.mute = function(){
-    this.media.muted = true;
+  this._initPugins();
 }
 
-MediaPlayer.prototype.unmute = function(){
-    this.media.muted = false;
-}
+MediaPlayer.prototype._initPugins = function () {
+  const player = {
+    play: () => this.play(),
+    pause: () => this.pause(),
+    media: this.media,
+    get muted() {
+      return this.media.muted;
+    },
 
-MediaPlayer.prototype.toggleMute = function(){
-    this.media.muted ? this.unmute() : this.mute();
+    set muted(value) {
+      this.media.muted = value;
+    },
+  };
+
+  this.plugins.forEach((plugin) => {
+    plugin.run(player);
+  });
+};
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  this.media.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  this.media.muted ? this.unmute() : this.mute();
 };
 
 export default MediaPlayer;
