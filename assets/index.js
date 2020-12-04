f85b4f18c55751884d5ee61d846dab4c6038b559
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-12-03 22:07:01 -0400

    added sw.js

--- assets/index.js
+++ assets/index.js
@@ -12,3 +12,8 @@ const btnMuteUnmute = document.querySelector("#btnMuteUnmute");
 
 btnPlayPause.onclick = () => player.togglePlay();
 btnMuteUnmute.onclick = () => player.toggleMute();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch((error) => {
+    console.log(error.message);
+  });
+}
---
+++ sw.js
@@ -0,0 +1,43 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  // get
+  if (request.method != "GET") {
+    return;
+  }
+
+  // Finds in cache.
+  event.respondWith(cacheResponse(request));
+  // Actualizar el cache
+  event.waitUntil(updateCache(request));
+});
+
+async function precache() {
+  const cache = await caches.open("v1");
+  return cache.addAll([
+    "/",
+    "/index.html",
+    "/assets/index.js",
+    "/assets/MediaPlayer.js",
+    "/assets/plugins/AutoPlay.js",
+    "/assets/plugins/AutoPause.js",
+    "/assets/index.css",
+    "/assets/BigBuckBunny.mp4",
+  ]);
+}
+
+async function cacheResponse(request){
+  const cache = await caches.open("v1");
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+async function updateCache(request){
+  const cache = await caches.open("v1");
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
