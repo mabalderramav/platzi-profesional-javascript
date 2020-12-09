716fc521ec637288f10a94c043e6e1e734c7511b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-12-08 21:34:57 -0400

    added basic type

--- Ejercicios/typescript/index.ts
+++ Ejercicios/typescript/index.ts
@@ -1,7 +1,51 @@
+// Boleand
+let muted: boolean = true;
+muted = false
+
+//Number
+let numerador: number = 8;
+let denominador: number = 2;
+let resultado: number = numerador / denominador;
+
+//String
+let helloWord: string = "Aldo";
+helloWord = `hola ${helloWord}`
+
+//Arrays
+let people: string[] = [];
+people = ["aldo","oscar","martin"];
+people.push("30");
+
+let peopleAndNumber: Array<string | number> = [];
+peopleAndNumber.push("aldo");
+peopleAndNumber.push(901);
+
+//Enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
+    Amarillo = "Amarillo",
+}
+
+let coloFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es ${coloFavorito}`);
+
+//Any
+let comodin: any = "aldo";
+comodin = {
+    type: "helki",
+}
+
+//Object
+let someObject: object = {
+    type: "helki",
+}
+
 console.log("hello typescript");
 
 function add(a: number,b: number){
-    return a+b;
+    return a + b;
 }
 
 let sum = add(2,3);
