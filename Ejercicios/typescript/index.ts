47a3dc24b454d3f14aaa9855e8a2b5d72a0c4c71
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-01-12 10:37:36 -0400

    typescript function

--- Ejercicios/typescript/index.ts
+++ Ejercicios/typescript/index.ts
@@ -44,9 +44,24 @@ let someObject: object = {
 
 console.log("hello typescript");
 
-function add(a: number,b: number){
+function add(a: number,b: number): number{
     return a + b;
 }
 
 let sum = add(2,3);
 console.log(sum);
+
+function createAdder(a: number): (number) => number{
+    return function (b: number){
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName?: string): string{
+    return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Aldo');
