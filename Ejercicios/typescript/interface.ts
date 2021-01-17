047b3f66c4f6f075bfaaf75a02b8d8c7ea3d454b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-01-17 18:27:53 -0400

    Added interface logic

---
+++ Ejercicios/typescript/interface.ts
@@ -0,0 +1,29 @@
+// Interface
+enum Color{
+    Rojo = 'Rojo',
+    Verde = 'Verde'
+}
+interface Rectangulo{
+    ancho: number
+    alto: number
+    color?: Color
+}
+
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    // color: Color.Verde
+}
+
+function area(r: Rectangulo): number{
+    return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function(){
+    return this.color ?  `Un rectangulo ${this.color}` : `Un rectangulo`;
+}
+
+console.log(rect.toString());
